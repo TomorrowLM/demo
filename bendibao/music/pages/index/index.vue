a50bd28a1ce45f37b5bd8af8f8dcf776547649f5
tom <120329698@qq.com>
2021-06-16 17:50:37 +0800

    window-6-16

--- bendibao/music/pages/index/index.vue
+++ bendibao/music/pages/index/index.vue
@@ -22,8 +22,13 @@
 							{{cardList[0][0]}}
 						</div>
 						<div>
-							{{cardList[0][1]}}
+							<span @click="showArea()">{{cardList[0][1]}}</span>
 							<img :src="cardList[0][2]" alt="">
+							<ul class="card-address" :class="{'show':cardAreaIsDrop}">
+								<li v-for="(item,index) in cardAddress" @click="selectArea(item)">
+									{{item}}
+								</li>
+							</ul>
 						</div>
 					</div>
 					<div class="card-body flex">
@@ -34,7 +39,7 @@
 						{{cardList[2][0]}}
 					</div>
 					<div class="card-position">
-						{{cardList[3][0]}}
+						{{cardAreaCul}}
 					</div>
 				</div>
 			</div>
@@ -102,14 +107,12 @@
 					节目介绍
 				</div>
 				<div class="introduce-info">
-					<div>1. 音乐节将依照要求严格控制每日观众入场人数。</div>
-					<div> 2. 检票入场时需要出示本人身份证，通过核实验证有效后方可入场，非入场人本人身份证会被谢绝入场。
-					</div>
-					<div> 3. 请配合现场工作人员的其他相关防疫检查，同时您 需要佩戴好口罩，配合测量体温合格后方可入场，活动全程也请佩戴好口罩，并保持安全距离。
+					<div v-for="(item,index) in introduceInfo" class=" animated fadeInLeft">
+						{{item}}
 					</div>
 				</div>
-				<div class="introduce-btn">
-					展开更多
+				<div class="introduce-btn" @click="showIntroduceInfo()">
+					{{introduceBtn}}
 				</div>
 				<div class="line"></div>
 				<div class="whole-team">
@@ -176,12 +179,12 @@
 				<div class="tab-recommend flex">
 					<div v-for="(item,index) in recommendList" @click="recommendSwitch(index)">
 						{{item}}
-						
+
 					</div>
 					<div class="progress animated fadeInLeft" ref="progress"></div>
 				</div>
 				<div class="line-2">
-					
+
 				</div>
 				<div class="music-news" v-show="recommendCul== 0">
 					<div v-for="(item,index) in musicNewsList">
@@ -208,6 +211,9 @@
 				areaCul: "武汉",
 				areaList: ["成都", "长沙", "上海"],
 				areaIsDrop: false,
+				cardAddress: ["1区", "2区", "3区"],
+				cardAreaCul: "1区",
+				cardAreaIsDrop: false,
 				cardList: [
 					["M_DSK", "武汉", "../../static/形状 1 拷贝@3x.png"],
 					["2021.04.07 周六 13:30", "../../static/形状 4.png"],
@@ -232,23 +238,29 @@
 					["关于票面价", "个别节目票纸上体现的价格与详情页选择的【票面价】可能存在差别（通常会低于票面价），但该票对应的实际价格档位座位均不受影响，如介意请谨慎购买。"],
 					["条件退款", "项目销售业有“条件退款”标签的项目，如因买家原因无法正常观演，在扣除实际支付金额一定比例的手续费后，可以进行退票。"]
 				],
-				noticeList:[
-					["节目时长","以现场为准"],
-					["入场时间","以现场为准"],
-					["条件退款","演出现场禁止携带违禁食品、管制刀具、打火机、旗杆、冷焰火、宠物、各类瓶装饮料、含酒精制品、航拍飞行器、帐篷、充气沙发等物品。"],
-					["寄存说明","以现场为准"]
+				introduceInfo: [
+					"1. 音乐节将依照要求严格控制每日观众入场人数。",
+					" 2. 检票入场时需要出示本人身份证，通过核实验证有效后方可入场，非入场人本人身份证会被谢绝入场。",
+					" 3. 请配合现场工作人员的其他相关防疫检查，同时您 需要佩戴好口罩，配合测量体温合格后方可入场，活动全程也请佩戴好口罩，并保持安全距离。"
 				],
-				questionBarList:["购票需要登记身份证吗？","购票需要登记身份证吗？","有没有现场电话号码？","没有大陆身份证怎么订票？","有座位图可以查看吗？","上门取票地址在哪里？"],
-				recommendCul:0,
-				recommendList:["音乐节","赏花咨讯","踏青资讯","春游资讯","疫情资讯"],
-				musicNewsList:[
-					["武汉音乐节2021时间表（持续更新）","2021-04-08 13:56"],
-					["2021武汉MDSK音乐节嘉宾阵容名单一览","2021-05-13 13:41"],
-					["2021武汉MDSK音乐节嘉宾阵容名单一览","2021-05-13 13:41"],
-					["2021武汉MDSK音乐节嘉宾阵容名单一览","2021-05-13 13:41"],
-					["2021武汉MDSK音乐节嘉宾阵容名单一览","2021-05-13 13:41"],
+				introduceBtn: "展开更多",
+				noticeList: [
+					["节目时长", "以现场为准"],
+					["入场时间", "以现场为准"],
+					["条件退款", "演出现场禁止携带违禁食品、管制刀具、打火机、旗杆、冷焰火、宠物、各类瓶装饮料、含酒精制品、航拍飞行器、帐篷、充气沙发等物品。"],
+					["寄存说明", "以现场为准"]
 				],
-				newsBtn:"查看更多"
+				questionBarList: ["购票需要登记身份证吗？", "购票需要登记身份证吗？", "有没有现场电话号码？", "没有大陆身份证怎么订票？", "有座位图可以查看吗？", "上门取票地址在哪里？"],
+				recommendCul: 0,
+				recommendList: ["音乐节", "赏花咨讯", "踏青资讯", "春游资讯", "疫情资讯"],
+				musicNewsList: [
+					["武汉音乐节2021时间表（持续更新）", "2021-04-08 13:56"],
+					["2021武汉MDSK音乐节嘉宾阵容名单一览", "2021-05-13 13:41"],
+					["2021武汉MDSK音乐节嘉宾阵容名单一览", "2021-05-13 13:41"],
+					["2021武汉MDSK音乐节嘉宾阵容名单一览", "2021-05-13 13:41"],
+					["2021武汉MDSK音乐节嘉宾阵容名单一览", "2021-05-13 13:41"],
+				],
+				newsBtn: "查看更多"
 			}
 		},
 		mounted() {
@@ -263,6 +275,14 @@
 				this.areaIsDrop = false
 				let old = this.areaCul
 				this.areaCul = item
+			},
+			showArea() {
+				this.cardAreaIsDrop = !this.cardAreaIsDrop
+			},
+			selectArea(item) {
+				this.cardAreaIsDrop = false
+				let old = this.cardAreaCul
+				this.cardAreaCul = item
 			},
 			tabSwitch(index, location) {
 				this.tabCul = index;
@@ -286,26 +306,31 @@
 					this.$refs.tabList.classList.remove("fixed");
 				}
 			},
-			recommendSwitch(index){
-				this.$refs.progress.style.left= index*1.5+"rem"
-				if(index==0){
-				this.$refs.progress.style.left= 0.3+"rem"
+			recommendSwitch(index) {
+				this.$refs.progress.style.left = index * 1.5 + "rem"
+				if (index == 0) {
+					this.$refs.progress.style.left = 0.3 + "rem"
 				}
-				this.recommendCul=index;
+				this.recommendCul = index;
 			},
-			showMoreNews(){
-			
-				setTimeout(()=>{
-					if(this.musicNewsList.length<10){
-							console.log(1)
+			showIntroduceInfo() {
+				if (this.introduceInfo.length <= 5) {
+					this.introduceInfo.push("4. 音乐节将依照要求严格控制每日观众入场人数。")
+				} else
+					this.introduceBtn = "没有了"
+			},
+			showMoreNews() {
+				setTimeout(() => {
+					if (this.musicNewsList.length < 10) {
+						console.log(1)
 						let old = this.musicNewsList
-						old.forEach((val)=>{
+						old.forEach((val) => {
 							this.musicNewsList.push(val)
 						})
-					}else
+					} else
 						this.newsBtn = "没有了"
-					
-				},1000)
+
+				}, 1000)
 			}
 		}
 	}
---
+++ bendibao/疫苗接种记录--输入框筛选数据/pages/index/index.vue
@@ -0,0 +1,256 @@
+<template>
+	<div class="page">
+		<div class="header">
+			<img src="@/static/hearder.png" alt="">
+			<div class="select-drop">
+				<div class="selected" v-model="cityCul" @click="cityDrop">
+					{{cityCul}}
+					<img src="@/static/形状 7_1.png" alt="">
+				</div>
+				<div class="city-list" :class="{show:cityIsDrop}">
+					<div v-for="(item,index) in cityList" @click="cityRise(item)">
+						{{item}}
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="line-1">
+		</div>
+		<div class="main">
+			<div class="platform">
+				<div class="platform-title">
+					<img src="@/static/椭圆 1_1.png" alt="">
+					本地区查询平台
+				</div>
+				<div class="platform-card" v-for="(item,index) in platformList">
+					<div class="platform-card-container">
+						<div>
+							<span>{{item[0]}}</span>
+							<span>{{item[1]}}</span>
+						</div>
+						<div class="line-2"></div>
+						<div>
+							<div>{{item[2]}}</div>
+							<div>{{item[3]}}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="other-platform">
+				<div class="platform-title">
+					<img src="@/static/椭圆 1_1.png" alt="">
+					其他地区查询平台
+				</div>
+				<div class="search">
+					<input type="text" placeholder="请输入查询省份" v-model:value="searchVal">
+					<ul class="search-list animated fadeInUp" :class="{'search-list-show':searchList.length !== 0}">
+						<li v-for="(item,index) in searchList" @click="selectProvince(item)">{{item}}</li>
+					</ul>
+					<div>
+						<img src="@/static/搜索@3x.png" alt="" @click="searchProvince">
+					</div>
+				</div>
+				<div class="other-card" v-for="(item,index) in otherList" v-show="provinceCul==''">
+					<div class="other-card-container">
+						<div class="other-card-header flex">
+							<div>
+								<span>{{item[0]}}</span>
+								<span>{{item[1]}}</span>
+							</div>
+							<div>
+								<img src="@/static/形状 4 拷贝@2x.png" alt="">
+							</div>
+						</div>
+						<div class="other-card-body" v-for="(item,index) in otheAreaList">
+
+							<div>
+								<div>
+									<img src="@/static/圆角矩形 16.png" alt="">
+									{{item[0]}}
+								</div>
+								<div>
+									{{item[1]}}
+								</div>
+								<div>
+									{{item[2]}}
+
+								</div>
+							</div>
+							<hr v-show="index!==(otheAreaList.length-1)">
+
+						</div>
+					</div>
+				</div>
+
+				<div class="other-card" v-show="provinceCul==provinceCulList[0]">
+					<div class="other-card-container">
+						<div class="other-card-header flex">
+							<div>
+								<span>{{provinceCulList[0]}}</span>
+								<span>{{provinceCulList[1]}}</span>
+							</div>
+							<div>
+								<img src="@/static/形状 4 拷贝@2x.png" alt="">
+							</div>
+						</div>
+						<div class="other-card-body" v-for="(item,index) in otheAreaList">
+
+							<div>
+								<div>
+									<img src="@/static/圆角矩形 16.png" alt="">
+									{{item[0]}}
+								</div>
+								<div>
+									{{item[1]}}
+								</div>
+								<div>
+									{{item[2]}}
+
+								</div>
+							</div>
+							<hr v-show="index!==(otheAreaList.length-1)">
+
+						</div>
+					</div>
+				</div>
+				<div v-show="provinceCul=='no information'" class="selected-false">
+					抱歉，没有该城市的信息
+				</div>
+			</div>
+			<div class="module">
+				导粉模块
+			</div>
+		</div>
+		<div class="footer">
+			<div class="footer-container">
+				<div>
+					<div  class="btn">
+						<div>分享</div>
+						<img src="@/static/分 享.png" alt="">
+					</div>
+				</div>
+				<div>
+					跳转到医疗资讯页
+				</div>
+			</div>
+		</div>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				cityCul: "武汉",
+				cityList: [1, 2, 3],
+				cityIsDrop: false,
+				// 本地平台列表
+				platformList: [
+					["湖北疾控", "微信公众号", "查询入口", "查询指南"],
+					["湖北健康码", "支付宝", "查询入口", "查询指南"],
+					["鄂惠办", "微信小程序", "查询入口", "查询指南"],
+					["武汉战疫", "微信小程序", "查询入口", "查询指南"],
+				],
+				//展示其他平台列表
+				otherList: [
+					["广州", "共3个查询平台"],
+					["北京", "共3个查询平台"],
+					["上海", "共3个查询平台"]
+				],
+				//其他平台总的列表
+				otherList1: [
+					["广州", "共3个查询平台"],
+					["广西", "共3个查询平台"],
+					["北京", "共3个查询平台"],
+					["上海", "共3个查询平台"],
+					["成都", "共3个查询平台"],
+					["北京", "共3个查询平台"],
+					["武汉", "共3个查询平台"]
+				],
+				//其他地区的列表
+				otheAreaList: [
+					["广州疾控", "查询入口", "查询指南"],
+					["广州疾控", "查询入口", "查询指南"],
+					["广州疾控", "查询入口", "查询指南"]
+				],
+				//输入框的值
+				searchVal: "",
+				// 检索输入框的字符，得到的其他省份的列表
+				searchList: [],
+				//选中省份的值
+				provinceCul: "",
+				// 选中省份列表
+				provinceCulList: [],
+				
+			}
+		},
+		// 监听searchVal变化，将searchList展示
+		watch: {
+			searchVal(curVal, oldVal) {
+				this.searchList = []
+				let otherList1 = []
+				this.otherList1.forEach((value, index) => {
+					otherList1.push(value[0])
+				})
+				let reg = new RegExp(this.searchVal)
+				otherList1.forEach((value, index) => {
+					if (reg.test(value) && !reg.test("")) {
+						this.searchList.push(value)
+					}
+				})
+			}
+		},
+		methods: {
+			cityDrop() {
+				this.cityIsDrop = !this.cityIsDrop
+			},
+			cityRise(item) {
+				this.cityIsDrop = false
+				let old = this.cityCul
+				this.cityCul = item
+			},
+			// 选择searchList中的某个省份，展示provinceCulList
+			selectProvince(item) {
+				console.log(this.provinceCul)
+				this.searchVal = ""
+				this.provinceCul = item
+				this.otherList1.forEach((value, index) => {
+					if (value[0].indexOf(this.provinceCul) !== -1) {
+						this.provinceCulList = value
+					}
+				})
+
+			},
+			//点击搜索按钮，展示provinceCulList
+			searchProvince() {
+				console.log(this.provinceCul)
+				let otherList1 = []
+				this.searchList = []
+				this.provinceCul = ""
+				this.provinceCulList = []
+				this.otherList1.forEach((value, index) => {
+					otherList1.push(value[0])
+				})
+				let reg = new RegExp(this.searchVal)
+				otherList1.forEach((value, index) => {
+					if (value == this.searchVal) {
+						console.log(1)
+						this.provinceCul = value
+						this.provinceCulList = this.otherList1[index]
+					}
+				})
+				if (this.provinceCulList.length == 0) {
+					console.log(2)
+					this.provinceCul = "no information"
+				}
+				this.searchVal = ""
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("@/css/index.css");
+	@import url("@/common/animate/animate.min.css");
+</style>
