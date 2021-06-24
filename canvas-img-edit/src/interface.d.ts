619cd4d51b2052d8b958340c325bfc557e41d937
tom <120329698@qq.com>
2021-06-24 18:02:50 +0800

    window-6-24

--- canvas-img-edit/src/interface.d.ts
+++
@@ -1,7 +0,0 @@
-interface CanvasRenderingContext2D {
-  backingStorePixelRatio?: number;
-  webkitBackingStorePixelRatio?: number;
-  mozBackingStorePixelRatio?: number;
-  msBackingStorePixelRatio?: number;
-  oBackingStorePixelRatio?: number;
-};
