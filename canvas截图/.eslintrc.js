eea21f6a6fcc6ab6d1b7122eb3514db02aa4a683
root <root@LM.localdomain>
2021-07-07 23:59:54 +0800

    window-7-8

--- canvas截图/.eslintrc.js
+++ canvas截图/.eslintrc.js
@@ -3,14 +3,17 @@ module.exports = {
     "env": {
         "browser": true,
         "es2021": true,
-        "es6": true,
         "commonjs": true,
     },
     "extends": "eslint:recommended",
+    "plugins": ["babel"],
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaVersion": 12
     },
-    "rules": {
-        "semi": 2    //语句强制分号结尾
+    "rules": { 
+        "semi": 2,    //语句强制分号结尾
+        "no-console": 1,
+        "quotes": ["error", "double"],
     }
 };
