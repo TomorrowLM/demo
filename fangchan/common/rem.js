4f38d80d631d7be61fe13a2743c1315dbe7b8919
root <root@LM.localdomain>
2021-07-23 00:04:20 +0800

    window-7-13

--- fangchan/common/rem.js
+++ fangchan/common/rem.js
@@ -1,19 +1,21 @@
-(function (doc, win) {
-  var docEl = doc.documentElement,
-    resizeEvt = "orientationchange" in window ? "orientationchange" : "resize",
-    recalc = function () {
-      var clientWidth = docEl.clientWidth;
-      if (!clientWidth) return;
-      if (1500 < clientWidth && clientWidth <= 1920) {
-        docEl.style.fontSize = 100 * (clientWidth / 1920) + "px";
-      } else if (clientWidth <= 1500) {
-        console.log(clientWidth)
-        // docEl.style.fontSize = 100 * (clientWidth / 1920) + "px";
-        docEl.style.fontSize = "100px";
-      }
-    };
 
-  if (!doc.addEventListener) return;
-  win.addEventListener(resizeEvt, recalc, false);
-  doc.addEventListener("DOMContentLoaded", recalc, false);
-})(document, window);
+  (function (doc, win) {
+    var docEl = doc.documentElement,
+      resizeEvt = "orientationchange" in window ? "orientationchange" : "resize",
+      recalc = function () {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) return;
+        if (1400 < clientWidth && clientWidth <= 1920) {
+          docEl.style.fontSize = 100 * (clientWidth / 1920) + "px";
+          // docEl.style.fontSize = "100px";
+        } else if (clientWidth <= 1400) {
+          console.log(clientWidth)
+          // docEl.style.fontSize = 100 * (clientWidth / 1920) + "px";
+          docEl.style.fontSize = "100px";
+        }
+      };
+  
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener("DOMContentLoaded", recalc, false);
+  })(document, window);
