fba7ad711f38a1d415b63c1ad2a4f02015979be8
tom <12039698@qq.com>
2022-07-12 11:55:52 +0800

    vue:权限

--- web练习/ajax/新建文件夹/ajax/secondday/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $_GET['username'];
-$password = $_GET['password'];
-
-//$username = $_POST['username'];
-//$password = $_POST['password'];
-//echo '用户名:'.$username.'密码:'.$password;
-echo 1;
-?>
--- web练习/ajax/新建文件夹/php/cainiao/02.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Credentials:true');
-//setcookie('key','value');
-$a=$_COOKIE["user"];
-$b=$_COOKIE["pw"];
-
-echo $a+$b;
-
-?>
