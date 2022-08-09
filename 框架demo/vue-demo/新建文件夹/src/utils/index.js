e2d0fbc54e6e881887266ee69c4df667b3ae4fcf
TomorrowLM <120329698@qq.com>
2022-08-09 17:07:00 +0800

    地图初始化

--- 框架demo/vue-demo/新建文件夹/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-import './rem'
-import './base.js'
-import "./lazy_antdv"
-import "./storage"
-// import "./lazy_vant"
-// import "./lazy_element"
--- 框架demo/vue-demo/新建文件夹/src/utils/lazy_antdv.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue';
-import { Form } from 'ant-design-vue'
-Vue.prototype.$form = Form
