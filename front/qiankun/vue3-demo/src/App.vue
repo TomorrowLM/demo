73126c43567f0638021e871f51e9a48bb6bdb18a
liming <ming.li@cisdi.com.cn>
2024-07-01 16:13:37 +0800

    文件重构

---
+++ front/qiankun/vue3-demo/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app h100">
+    <header class="d-l-center d-r-end">
+      <span @click="goHome" class="float-right point">回到首页</span>
+    </header>
+    <RouterView />
+  </div>
+</template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+const jump = () => {
+  router.push('/3D')
+}
+let theme = window.localStorage.getItem('skin') || 'light'
+// if (theme === 'light') {
+//   theme = 'dark'
+//   window.localStorage.setItem('skin', 'dark')
+// } else {
+//   theme = 'light'
+//   window.localStorage.setItem('skin', 'light')
+// }
+onBeforeMount(() => {
+  // document.documentElement.setAttribute('theme', window.localStorage.getItem('skin') || 'light')
+})
+const goHome = () => {
+  console.log(router)
+  router.push({
+    name: 'home'
+  })
+}
+console.log(56)
+</script>
+
+<style lang="scss">
+#__qiankun_microapp_wrapper_for_vue_3__,
+#vue3-page {
+  height: 100%;
+}
+header {
+  color: $text;
+  // background: $header-bg;
+  height: 48px;
+  padding: 16px;
+  box-sizing: border-box;
+  border-bottom: 1px solid $header-bg;
+  box-shadow: 1px 1px 5px $shadow;
+}
+</style>
