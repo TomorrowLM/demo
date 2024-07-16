2e670bddf019f1134da42b99c2aa59d5540626c1
liming <ming.li@cisdi.com.cn>
2024-07-16 15:52:55 +0800

    wps1

--- front/qiankun/html-demo/wps/index.js
+++ front/qiankun/html-demo/wps/index.js
@@ -1,103 +1,142 @@
-console.log(); 
 // 请求参数 
-const url = window.location.origin + `/qt-wps/files/operate?fileId=1811061490312192000&operateType=write`; 
-console.log(url); 
-const config={  
-  url,  
-  method:'get', //默认get 
-  headers:{}, // 请求头 
+const url = window.location.origin + `/qt-wps/files/operate?fileId=1811061490312192000&operateType=write`;
+console.log(url);
+const config = {
+  url,
+  method: 'get', //默认get 
+  headers: {}, // 请求头 
   params: {}, // GET请求时参数 
-  data:{}, // POST请求时参数 
-  responseType:'json' //默认返回json 
-}; 
+  data: {}, // POST请求时参数 
+  responseType: 'json' //默认返回json 
+};
 // 公用请求 
-let link; 
-new Promise(resolved=>{ 
-  resolved(this.$eapHttp(config)) 
-}).then(async res=>{ 
-  console.log(res); 
-  link = res.data.link; 
-  console.log(link); 
-  const instance =await WebOfficeSDK.config({ 
+let link;
+new Promise(resolved => {
+  resolved(this.$eapHttp(config))
+}).then(async res => {
+  link = res.data.link;
+  const instance = await WebOfficeSDK.config({
     url: link, // 该地址需要对接方服务端提供，形如 https://wwo.wps.cn/office/p/xxx 
-    mount: document.querySelector('.cus-wps-box'), 
-    mode: 'nomal', 
-  }) 
-  await instance.ready() 
-  const app = instance.Application; 
-  console.log(app,111,app.ActivePresentation); 
+    mount: document.querySelector('.cus-wps-box'),
+    mode: 'nomal',
+  })
+  //初始化
+  await instance.ready()
+  const app = instance.Application;
   // 评论对象 
-  const comments = await app.ActiveDocument.Comments; 
-  console.log(comments); 
-// 获取修订对象 
-  const revisions = await app.ActiveDocument.Revisions; 
-console.log('修订',revisions); 
-  const commentsInfo = [ 
-    { 
-      Range: { 
-        Start: 0, 
-        End: 9 
-      }, 
-      Text: 'WebOffice 评论' 
-    } 
-  ] 
+  const comments = await app.ActiveDocument.Comments;
+  // 获取修订对象 
+  const revisions = await app.ActiveDocument.Revisions;
+  console.log('修订', revisions);
+  const commentsInfo = [
+    {
+      // Range: {
+      //   Start: 0,
+      //   End: 9
+      // },
+      searchText: '三',//文本内容
+      index: 1,//索引
+      commentText: 'WebOffice 评论',//评论
+      revision: '四'
+    }
+  ]
   // 是否有评论 
-  const hasComments = await app.ActiveDocument.HasComments(); 
- 
-  if(!hasComments){ 
+  const hasComments = await app.ActiveDocument.HasComments();
+  let rightDom = '';//
+
+  for (let i = 0; i < commentsInfo.length; i++) {
+    //搜索文本 
+    await app.ActiveDocument.Find.Execute(commentsInfo[i].searchText, false).then(async res => {
+      console.log(res);
+      // const range = app.ActiveDocument.Range.SetRange(res.pos, res.pos + res.len);
+      commentsInfo[i].Range = {
+        Start: res[commentsInfo[i].index].pos,
+        End: res[commentsInfo[i].index].pos + res[commentsInfo[i].index].len
+      }
+      // 获取选中区域 
+      const range = await app.ActiveDocument.Range(commentsInfo[i].Range.Start, commentsInfo[i].Range.End);
+      // 字体对象 
+      const font = await app.ActiveDocument.Range(commentsInfo[i].Range.Start, commentsInfo[i].Range.End).Font;
+      font.Color = '#000';
+      // 设置选定文字的背景颜色 
+      font.HighLight = '#F78C6C';
+      rightDom += `<li>${i}<a type=1 index=${i}>跳转</a ><a type=2 index=${i}>修订</a ></li>`;
+    });
+  }
+  // setTimeout(async () => {
+  //   console.log(55);
+  //   for (let i = 0; i < commentsInfo.length; i++) {
+  //     // 字体对象 
+  //     const font = await app.ActiveDocument.Range(commentsInfo[i].Range.Start, commentsInfo[i].Range.End).Font;
+  //     console.log(font, 123, commentsInfo[i].Range);
+  //     font.Color = '#F78C6C';
+  //     // 设置选定文字的背景颜色 
+  //     font.HighLight = '#F78C6C';
+  //   }
+  // }, 10);
+  console.log(commentsInfo); //TODO: 异步
+  if (!hasComments) {
     // 添加评论 
-    commentsInfo.forEach(async val=>{ 
-      await comments.Add(val); 
-    }) 
-  }else{ 
-    console.log('有评论'); 
+    commentsInfo.forEach(async val => {
+      await comments.Add({
+        Range: val.Range,
+        Text: val.commentText
+      });
+    })
+  } else {
+    console.log('有评论');
     // 删除文档内的所有评论 
-  // await app.ActiveDocument.DeleteAllComments() 
-  } 
-    commentsInfo.forEach(async val=>{ 
-      console.log('range',); 
-     // 获取选中区域 
-      const range =  app.ActiveDocument.Range(val.Range.Start, val.Range.End); 
-      // 设置文本 
-      range.Text = 'WebOffice11'; 
-        // 字体对象 
-    const font = await app.ActiveDocument.Range(val.Range.Start, val.Range.End).Font; 
-   
-  // 设置选定文字的字体颜色 
-    font.Color = '#05aae1'; 
-    font.HighLight = '#05aae1'; 
-    }) 
-  let rightDom = '' 
+    // await app.ActiveDocument.DeleteAllComments() 
+  }
+
   // 获取全文评论 
-  const operatorsInfo = await app.ActiveDocument.GetComments({ Offset: 0, Limit: null }); 
-  console.log(operatorsInfo); 
-   
-  operatorsInfo.forEach(val=>{ 
-    console.log(val); 
-    rightDom+=`<li>${val.rcId}<a  id=${val.rcId}>跳转</a ></li>` 
-  }) 
-  document.querySelector('.cus-wps-right').innerHTML = rightDom; 
-  document.querySelector('.cus-wps-right').addEventListener('click', async (event)=>{ 
-            var target = event.target || event.srcElement; 
-          console.log(target,target.getAttribute('id'),comments); 
-      // 获取range 
-  const range = await app.ActiveDocument.Range.SetRange(122,123); 
-  console.log(range) 
- 
-  // 滚动文档窗口, 显示指定的区域 
-  // await app.ActiveDocument.ActiveWindow.ScrollIntoView(range) 
+  // const operatorsInfo = await app.ActiveDocument.GetComments({ Offset: 0, Limit: null });
+
+
+  // operatorsInfo.forEach(val => {
+  //   console.log(val);
+  //   rightDom += `<li>${val.rcId}<a  id=${val.rcId}>跳转</a ></li>`
+  // })
+  document.querySelector('.cus-wps-right').innerHTML = rightDom;
+  document.querySelector('.cus-wps-right').addEventListener('click', async (event) => {
+    var target = event.target || event.srcElement;
+    console.log(target, target.getAttribute('index'));
+    const index = target.getAttribute('index')
+    const type = target.getAttribute('type')
+    if (type === '1') {
+      //跳转
+      const range = await app.ActiveDocument.Range.SetRange(commentsInfo[index].Range.Start, commentsInfo[index].Range.End);
+      await app.ActiveDocument.ActiveWindow.ScrollIntoView(range);
+      // 字体对象 
+      // const font = await app.ActiveDocument.Range.SetRange(commentsInfo[index].Range.Start, commentsInfo[index].Range.End).Font;
+      // console.log(font);
+      // font.Color = '#000';
+      // // 设置选定文字的背景颜色 
+      // font.HighLight = '#F78C6C';
+    } else {
+      //修订
+      // 获取选中区域 
+      const range = await app.ActiveDocument.Range(commentsInfo[index].Range.Start, commentsInfo[index].Range.End);
+      console.log('range', range);
+      // 设置文本 
+      range.Text = '四';
+      // 字体对象 
+      const font = await app.ActiveDocument.Range.SetRange(commentsInfo[index].Range.Start, commentsInfo[index].Range.End).Font;
+      font.Color = '#000';
+      // 设置选定文字的背景颜色 
+      font.HighLight = '#05aae1';
+    }
+    // 获取range 
+    // const range = await app.ActiveDocument.Range.SetRange(122, 123);
+    // console.log(range)
+
+    // 滚动文档窗口, 显示指定的区域 
+    // await app.ActiveDocument.ActiveWindow.ScrollIntoView(range) 
     // 搜索并高亮文本 
-  await app.ActiveDocument.Find.Execute('三', true).then(async res=>{ 
-    console.log(res); 
-    const range = await app.ActiveDocument.Range.SetRange(res[1].pos,res[1].pos+res[1].len); 
-    await app.ActiveDocument.ActiveWindow.ScrollIntoView(range) 
-  }); 
-  
-  // 查找对象 
-  const Find = await app.ActiveDocument.Find; 
-  console.log(Find.Execute('三', false)); 
-  },false); 
- 
-}); 
- 
- 
+    // await app.ActiveDocument.Find.Execute('三', true).then(async res => {
+    //   console.log(res);
+    // });
+
+  }, false);
+});
+
