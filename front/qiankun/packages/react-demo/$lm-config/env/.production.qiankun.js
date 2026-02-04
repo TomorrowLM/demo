2588e7b7ea63f46b0c25b623378f0e604e99d676
liming <120329698@qq.com>
2026-02-04 14:53:16 +0800

    优化

--- front/qiankun/packages/react-demo/$lm-config/env/.production.qiankun.js
+++ front/qiankun/packages/react-demo/$lm-config/env/.production.qiankun.js
@@ -6,6 +6,7 @@ export default {
   IS_PROD: true,
   IS_QIANKUN: true,
   APP_OUTPUTDIR: '../dist/qiankun/child/react-app',
-  APP_ROUTER_BASE: "/qiankun/react-app", // 微应用路由根路径
+  APP_ROUTER_BASE: "/", // 微应用路由根路径
+  APP_ROUTER_BASE_QIANKUN: "/qiankun/react-app/#/", // qiankun访问子应用的路由根路径
   Build_Path: '/qiankun/child/react-app/',
 }
