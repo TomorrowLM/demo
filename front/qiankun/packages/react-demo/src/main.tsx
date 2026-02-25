912a99a98f5019ebf287ac587790cd174f54baf0
liming <120329698@qq.com>
2026-02-25 17:46:35 +0800

    ts优化

--- front/qiankun/packages/react-demo/src/main.tsx
+++ front/qiankun/packages/react-demo/src/main.tsx
@@ -4,19 +4,13 @@ import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import store from "@/store";
 // import VConsole from 'vconsole';
-import { IsPC } from "@/utils";
 import App from './app'
 import './public-path';
 import { AppInit } from '@lm/shared';
 AppInit();
-// if (IsPC) {
-//   const vConsole = new VConsole();
-//   // 调用 console 方法输出日志
-//   console.log('Hello world');
-// }
 
 console.log('render.js', window.__POWERED_BY_QIANKUN__)
-function render(props) {
+function render(props: any) {
   const { container } = props;
   ReactDom.render(<Provider store={store}>
     <App />
@@ -32,21 +26,23 @@ if (!window.__POWERED_BY_QIANKUN__) {
   );
 }
 
-if (module && module.hot) {
-  module.hot.accept();
-}
+// if (module && module.hot) {
+//   // 热更新
+//   module.hot.accept();
+// }
 
 // qiankun 生命周期必须导出
 export const bootstrap = async () => {
   console.log('[react16] react app bootstraped');
 };
 
-export const mount = async (props) => {
+export const mount = async (props: any) => {
   console.log('[react16] props from main framework', props);
   render(props);
 };
 
-export const unmount = async (props) => {
+export const unmount = async (props: any) => {
   const { container } = props;
   ReactDom.unmountComponentAtNode(container ? container.querySelector('#react-app') : document.querySelector('#react-app'));
 };
+  
